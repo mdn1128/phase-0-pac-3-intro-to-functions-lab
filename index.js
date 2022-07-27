b04547432a4d203b3e81788d76b1a33e2b766847
Marvin Medina <mdn_1128@yahoo.com>
2022-07-26 20:11:09 -0700

    commit tree 3cd38712711b5292b47dcb7e27f60261a012dad6

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+const hello = "hello";
+function logShout(hello) {
+    return hello.toUpperCase();
+}
+console.log(hello.toUpperCase());
